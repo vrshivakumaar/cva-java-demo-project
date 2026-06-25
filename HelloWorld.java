c89ae3488cfa15d17289734de45888c4b2614570
Shiva <vrshivakumaar@gmail.com>
2026-06-25 09:50:03 +0000

    first commit of helloworld.java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // This prints a message to the console
-        System.out.println("Hello, World!");
+        System.out.println("welcome to my first devops code checkin");
     }
-
 
